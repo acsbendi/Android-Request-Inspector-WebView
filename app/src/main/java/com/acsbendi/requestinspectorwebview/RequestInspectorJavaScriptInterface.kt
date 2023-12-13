534bd0093259bc23d2ed0550ec414a105031a7cb
Erdem Tsynduev <erdemtsynduev@gmail.com>
2023-12-13 14:18:57 +0300

    Fixed interception of POST requests for 3DS
    A bug has been discovered where some requests are not correctly searching for the request body by URL.

--- app/src/main/java/com/acsbendi/requestinspectorwebview/RequestInspectorJavaScriptInterface.kt
+++ app/src/main/java/com/acsbendi/requestinspectorwebview/RequestInspectorJavaScriptInterface.kt
@@ -19,7 +19,11 @@ internal class RequestInspectorJavaScriptInterface(webView: WebView) {
 
     fun findRecordedRequestForUrl(url: String): RecordedRequest? {
         return synchronized(recordedRequests) {
-            recordedRequests.find { recordedRequest ->
+            recordedRequests.findLast { recordedRequest ->
+                // Added search by exact URL to find the actual request body
+                url == recordedRequest.url
+            } ?: recordedRequests.findLast { recordedRequest ->
+                // Previously, there was only a search by contains, and because of this, sometimes the wrong request body was found
                 url.contains(recordedRequest.url)
             }
         }
